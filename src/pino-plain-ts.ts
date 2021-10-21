a81557e4216b117ca5f92f2f8e62ee7c1c3bfb62
Rob <rmuchall@protonmail.com>
2021-10-22 00:19:42 +0100

    Initial code commit

---
+++ src/pino-plain-ts.ts
@@ -0,0 +1,14 @@
+import pino from "pino";
+
+const pinoLog = pino({
+    name: "pino-test",
+    level: "trace",
+    transport: {
+        target: "pino-pretty",
+        options: {
+            colorize: true,
+        }
+    }
+});
+
+pinoLog.debug("This is a test (pino)");
